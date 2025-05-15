50c2887f861c5a0819a60748a48037e9c4077da1
Esteban Abehzele <esteabzl@gmail.com>
2025-05-15 21:07:16 +0200

    Sauvegarde de Circuit en Json dans un fichier et Circuit.allGates est devenu un HashMap
    Co-authored-by: Cupo2fleurs <Cupo2fleurs@users.noreply.github.com>

--- src/main/java/com/pjava/src/components/Circuit.java
+++ src/main/java/com/pjava/src/components/Circuit.java
@@ -1,9 +1,12 @@
 package com.pjava.src.components;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.BitSet;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import com.pjava.src.errors.BusSizeException;
 import com.pjava.src.utils.Cyclic;
@@ -17,12 +20,19 @@ import com.pjava.src.components.input.Power;
 import com.pjava.src.components.input.Input;
 import com.pjava.src.components.output.Output;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Circuit{
 
+    //#region Attributes
     /**
     * List of all the gates of a circuit
     */
-    public ArrayList<Gate> listGate = new ArrayList<Gate>();
+    //public ArrayList<Gate> allGates = new ArrayList<Gate>();
+    private HashMap<String,Gate> allGates = new HashMap<String,Gate>();
 
     // /**
     // * List of all the input gates of a circuit
@@ -34,140 +44,138 @@ public class Circuit{
     // */
     // private ArrayList<Output> circuitOutput = new ArrayList<Output>();
 
+    //#endregion
+
+
     public Circuit(){
     }
+
+
+    //#region Setter
+
+    //#endregion
+
+
+    //#region Getter
+    public HashMap<String,Gate> get_allGates(){
+        return this.allGates;
+    }
+    //#endregion
+
 
     /**
      *
      * @param type A string of the gate type to be created
+     * @param label The key for this gate
      * @retrun The gate that has just been created
      */
-    public Gate addGate(String type) throws Error{
+    public Gate addGate(String type, String label) throws Error{
+
         Gate newGate;
         switch(type){
             case "Power":
-                newGate = new Power();
-                break;
+            newGate = new Power();
+            break;
             case "Ground":
-                newGate = new Ground();
-                break;
+            newGate = new Ground();
+            break;
             case "And":
-                newGate = new And();
-                break;
+            newGate = new And();
+            break;
             case "Not":
-                newGate = new Not();
-                break;
+            newGate = new Not();
+            break;
             case "Or":
-                newGate = new Or();
-                break;
+            newGate = new Or();
+            break;
             default:
                 throw new Error(String.format("No match found for the string '%s'", type));
         }
-        listGate.add(newGate);
+
+        if(label == ""){
+            label = newGate.uuid().toString();
+        }
+
+        if(allGates.containsKey(label)){
+            throw new Error(String.format("This label is already taken : '%s' (creating gate : '%s')", label, type));
+        }
+
+        this.allGates.put(label,newGate);
 
         return newGate;
     }
 
-    public void startSimulation(){
+    public Gate addGate(String type){
+        return addGate(type, "");
     }
+
+
+
+    // TODO : startSimulation
+    public void startSimulation() {
+    }
+
+
+    public JSONObject toJson() {
+        // adding gates within the circuit to a JSON array
+        JSONArray gate_Array = new JSONArray();
+        for(Gate gate : allGates.values()){
+            gate_Array.put(gate.toJson());
+        }
+
+        // Creating the JSON object
+        JSONObject circuit_Json = new JSONObject();
+        circuit_Json.put("Gate",gate_Array);
+
+        return circuit_Json;
+    }
+
 
     public static void saveCircuit(Circuit circuit,String fileName){
+        try (FileWriter writer = new FileWriter(fileName)){
+            JSONObject circuit_Json = circuit.toJson();
+            writer.write(circuit_Json.toString(1));
+            System.out.println("Circuit save with succes in:" + fileName);
 
+        }
+        catch(IOException e){
+            System.err.println("Error" + fileName +"can't be save" + e.getMessage());
+        }
     }
-    // public static void saveCircuit(Circuit_bis circuit_bis, String fileName) {
-    //     try (FileWriter writer = new FileWriter(fileName)) {
-    //         JSONObject circuit_bisJson = circuit_bis.toJson();
-    //         writer.write(circuit_bisJson.toString(2)); // Indentation de 2 espaces pour lisibilité
-    //         System.out.println("Circuit_bis sauvegardé avec succès en JSON dans " + fileName);
 
-    //         // Afficher le chemin absolu
-    //         File file = new File(fileName);
-    //         System.out.println("Chemin absolu du fichier : " + file.getAbsolutePath());
-    //     } catch (IOException e) {
-    //         System.err.println("Erreur lors de la sauvegarde du circuit_bis en JSON: " + e.getMessage());
-    //         e.printStackTrace();
+
+
+
+
+    // public void load(JSONObject circuit_Json){
+    //     try {
+    //     Circuit circuit = new Circuit();
+    //     JSONArray gate_Array = circuit_Json.getJSONArray("Gate");
+    //     for (int i = 0; i< gate_Array.length(); i++){
+    //         JSONObject gate_Json = gate_Array.getJSONObject(i);
+    //         String type = gate_Json.getString("type");
+    //         circuit.addGate(type);
+    //         System.out.println("Circuit Load with succes");
     //     }
+    //     } catch (JSONException e) {
+    //         System.err.println("Error circuit can't be launch " + e.getMessage());
+    //     }
+
+    //     // 2 boucle, connection entre tous les gates
     // }
 
-
-    // public void loadCircuit(String pathFile){
-
-    //     // Lire le fichier JSON
-    //         StringBuilder content = new StringBuilder();
-    //         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-    //             String line;
-    //             while ((line = reader.readLine()) != null) {
-    //                 content.append(line);
-    //             }
+    // public void loadFromFile(String filePath){
+    //     StringBuilder content = new StringBuilder();
+    //     try (BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+    //         String line;
+    //         while ((line = reader.readLine()) != null) {
+    //             content.append(line);
     //         }
-    // }
-    // public void loadCircuit(JSonObject save) {
-    //     try{
-
-
-    //         // Parser le JSON
-    //         JSONObject circuit_bisJson = new JSONObject(content.toString());
-    //         Circuit_bis circuit_bis = new Circuit_bis();
-
-    //         // Créer d'abord tous les composants
-    //         JSONArray component_bissArray = circuit_bisJson.getJSONArray("component_biss");
-    //         for (int i = 0; i < component_bissArray.length(); i++) {
-    //             JSONObject component_bisJson = component_bissArray.getJSONObject(i);
-    //             String type = component_bisJson.getString("type");
-    //             String id = component_bisJson.getString("id");
-
-    //             Component_bis component_bis = null;
-    //             switch (type) {
-    //                 case "Power_bis":
-    //                     Power_bis power_bis = new Power_bis();
-    //                     ReflectionUtil_bis.setField(power_bis, "id", id);
-    //                     component_bis = power_bis;
-    //                     break;
-    //                 case "Ground_bis":
-    //                     Ground_bis ground_bis = new Ground_bis();
-    //                     ReflectionUtil_bis.setField(ground_bis, "id", id);
-    //                     component_bis = ground_bis;
-    //                     break;
-    //                 case "And_bis":
-    //                     And_bis and_bis = new And_bis();
-    //                     ReflectionUtil_bis.setField(and_bis, "id", id);
-    //                     component_bis = and_bis;
-    //                     break;
-    //                 case "Not_bis":
-    //                     Not_bis not_bis = new Not_bis();
-    //                     ReflectionUtil_bis.setField(not_bis, "id", id);
-    //                     component_bis = not_bis;
-    //                     break;
-    //             }
-
-    //             if (component_bis != null) {
-    //                 circuit_bis.addComponent_bis(component_bis);
-    //             }
-    //         }
-
-    //         // Puis établir les connexions
-    //         JSONArray connection_bissArray = circuit_bisJson.getJSONArray("connection_biss");
-    //         for (int i = 0; i < connection_bissArray.length(); i++) {
-    //             JSONObject connection_bisJson = connection_bissArray.getJSONObject(i);
-    //             String sourceId = connection_bisJson.getString("sourceId");
-    //             String targetId = connection_bisJson.getString("targetId");
-    //             int sourcePin = connection_bisJson.getInt("sourcePin");
-    //             int targetPin = connection_bisJson.getInt("targetPin");
-
-    //             Component_bis source = circuit_bis.getComponent_bisById(sourceId);
-    //             Component_bis target = circuit_bis.getComponent_bisById(targetId);
-
-    //             if (source != null && target != null) {
-    //                 source.connect(target, sourcePin, targetPin);
-    //             }
-    //         }
-
-    //         System.out.println("Circuit_bis chargé avec succès depuis JSON: " + fileName);
-    //         return circuit_bis;
-    //     } catch (IOException | JSONException e) {
-    //         System.err.println("Erreur lors du chargement du circuit_bis depuis JSON: " + e.getMessage());
-    //         e.printStackTrace();
-    //         return null;
+    //         JSONObject circuit_Json = new JSONObject(content.toString());
+    //         this.load(circuit_Json);
+    //     }catch (IOException e){
+    //         System.err.println("Error circuit can't be launch " + e.getMessage());
     //     }
+
     // }
 }
--- src/test/java/com/pjava/components/CircuitTest.java
+++ src/test/java/com/pjava/components/CircuitTest.java
@@ -1,5 +1,7 @@
 package com.pjava.components;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,8 +14,8 @@ import com.pjava.src.utils.*;
 public class CircuitTest{
 
     /**
-     *  On test toJson() avec un gate
-     *  On print le résultat
+     *  We try '.toJson()' with a gate
+     *  We print the result
      */
     @Test
     void test1(){
@@ -22,6 +24,10 @@ public class CircuitTest{
         System.out.println(and.toJson().toString());
     }
 
+    /**
+     *  We try adding gates in a 'circuit' instance
+     *  Then we print the result
+     */
     @Test
     void test2(){
         System.out.println("\ntest 2 :");
@@ -29,17 +35,39 @@ public class CircuitTest{
 
 
         circuit.addGate("And");
-        circuit.addGate("Or");
+        circuit.addGate("Or", "or1");
+        circuit.addGate("Not", "not1");
+        circuit.addGate("Not", "not2");
+        circuit.addGate("Not", "not3");
         circuit.addGate("Not");
         circuit.addGate("Power");
         circuit.addGate("Ground");
 
+
+        System.out.println("circuit to JSON by hand :");
         int j = 0;
-        for(Gate i : circuit.listGate){
-            System.out.println(String.format("%d : %s", j, i.toJson().toString()));
+        for(String i : circuit.get_allGates().keySet()){
+            System.out.println(String.format("%d : %s : %s", j, i, circuit.get_allGates().get(i).toJson()));
             j++;
         }
 
+        System.out.println("\ncircuit to JSON with Circuit.toJSON :");
+        System.out.println(circuit.toJson().toString(1));
+
+    }
+
+    @Test
+    void test3(){
+        System.out.println("\ntest 3 :");
+        Circuit circuit2 = new Circuit();
+
+        circuit2.addGate("And");
+        circuit2.addGate("Or");
+        circuit2.addGate("Not");
+        circuit2.addGate("Power");
+        circuit2.addGate("Ground");
+
+        Circuit.saveCircuit(circuit2,"save/save1");
     }
 
 }
