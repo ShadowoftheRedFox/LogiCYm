9c50311ed3cb5cfec5f8f6460b9da5068ccb1678
Expert Daddy Pig <67962204+ExpertDaddyPig@users.noreply.github.com>
2025-05-16 20:13:00 +0200

    alo

--- src/test/java/com/pjava/MainTest.java
+++ src/test/java/com/pjava/MainTest.java
@@ -29,23 +29,27 @@ public class MainTest {
     @Test
     void basic() throws Exception {
         Power p1 = new Power(2);
+        Power p3 = new Power(2);
+        Power p4 = new Power(2);
         Not n1 = new Not(2);
         // Ground p1 = new Ground();
         // Power p2 = new Power();
         Ground p2 = new Ground(2);
 
         And and = new And(2);
-        Or or = new Or();
+        Or or = new Or(2);
         Not not = new Not(2);
 
         Display display = new Display(2, 2);
+        Display display2 = new Display(2, 2);
 
         and.connect(not);
         p1.connect(n1);
-        n1.connect(and);
         p2.connect(and);
+        n1.connect(and);
 
         ArrayList<Gate> gates = new ArrayList<Gate>();
+        ArrayList<Gate> gates2 = new ArrayList<Gate>();
 
         gates.add(n1);
         gates.add(p2);
@@ -54,9 +58,26 @@ public class MainTest {
 
         not.connect(display);
 
-        new Schema("test", gates).exportSchema();
+        Schema created = new Schema("test", gates);
 
-        new Schema("test2").importSchema("test");
+        p4.connect(created);
+        created.connect(or);
+        p3.connect(or);
+        or.connect(display2);
+
+        System.out.println(created.getInputNumber() + " " + created.getInputCable());
+        System.out.println(created.getInputBus().length + " " + p4.getOutputBus().length);
+
+        gates2.add(created);
+        gates2.add(p3);
+        gates2.add(or);
+
+
+        Schema created2 = new Schema("test2", gates2);
+        created2.exportSchema();
+
+        System.out.println(created.getGates());
+        System.out.println(created2.getGates());
 }
 
     @Test
